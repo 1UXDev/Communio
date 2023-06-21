4e8687e34de9362a0c1d4de748a2259711175cc1
mntzd <julien.leske@gmail.com>
2023-06-21 16:30:05 +0200

    minor changes & bug fixed

--- src/components/CardCarousel/CardCarousel.js
+++ src/components/CardCarousel/CardCarousel.js
@@ -19,25 +19,25 @@ export default function CardCarousel({ organization }) {
           return org.products.map((product) => {
             return (
               <li key={product.productId} className="small">
-                <Link href={`/products/${product.productId}`}>
-                  <IMGwrapper>
-                    <img src={product.productImage} alt={product.name} />
-                    <IMGoverlay>
-                      <img src="/plus.png" alt="add to cart" width="50px"></img>
-                      <h4>{org.name}</h4>
-                    </IMGoverlay>
-                  </IMGwrapper>
-                  <TEXTwrapper>
-                    <span>{product.pricePerPieceEuro.toFixed(2)}€</span>
-                    <h3>
-                      {product.name}, {product.weightSize} {product.unit}
-                    </h3>
-                    <p>
-                      Needed: {product.amountNeeded - product.amountSold}{" "}
-                      {product.unit}
-                    </p>
-                  </TEXTwrapper>
-                </Link>
+                {/* <Link href={`/products/${product.productId}`}> */}
+                <IMGwrapper>
+                  <img src={product.productImage} alt={product.name} />
+                  <IMGoverlay>
+                    <img src="/plus.png" alt="add to cart" width="50px"></img>
+                    <h4>{org.name}</h4>
+                  </IMGoverlay>
+                </IMGwrapper>
+                <TEXTwrapper>
+                  <span>{product.pricePerPieceEuro.toFixed(2)}€</span>
+                  <h3>
+                    {product.name}, {product.weightSize} {product.unit}
+                  </h3>
+                  <p>
+                    Needed: {product.amountNeeded - product.amountSold}{" "}
+                    {product.unit}
+                  </p>
+                </TEXTwrapper>
+                {/* </Link> */}
               </li>
             );
           });
