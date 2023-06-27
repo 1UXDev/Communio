10ccd88600dfe7055f4de48850f0ab9f98cb756d
mntzd <julien.leske@gmail.com>
2023-06-27 22:38:38 +0200

    mongo adapter switched with custom adapter, but not ideal because accounts is not saved, jsut users, this is basically a backup commit

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-
+import { createUser } from "../users/createUser";
 // // adapter
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongoose";
@@ -20,37 +20,38 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  adapter: MongoDBAdapter(clientPromise),
+  //adapter: MongoDBAdapter(clientPromise),
   pages: {
-    signIn: "/",
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      //users first singin?
+      if (account.provider === "github" || account.provider === "google") {
+        // create user in "users"-collection
+        await createUser({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          providerAccountId: user.providerAccountId,
+          image: user.image,
+          coordinates: [],
+          radiusInKM: null,
+          bezirk: "",
+          kiez: "",
+          street: "",
+          streetNumber: null,
+          plz: null,
+          preferredPaymentMethod: [],
+          paymentCache: [],
+          favOrgs: [],
+          purchaseHistory: [],
+          favoriteIceCream: "chocolate",
+        });
+      }
+      return true;
+    },
   },
 };
 
 export default NextAuth(authOptions);
-
-//   callbacks: {
-//     async signIn(user, account, profile) {
-//       // users first singin?
-//       if (account.provider === ("github" || "google") && user.sub) {
-//         // create user in "users"-collection
-//         await createUser({
-//           id: user.id,
-//           name: user.name,
-//           email: user.email, //new!
-//           image: user.image, //new!
-//           // coordinates: [],
-//           // radiusInKM: null,
-//           // bezirk: "",
-//           // kiez: "",
-//           // street: "",
-//           // streetNumber: null,
-//           // plz: null,
-//           // preferredPaymentMethod: [],
-//           // paymentCache: [],
-//           // favOrgs: [],
-//           // purchaseHistory: [],
-//         });
-//       }
-//     },
-//   },
-// };
--- src/pages/api/users/createUser.js
+++ src/pages/api/users/createUser.js
@@ -1,16 +1,23 @@
 import dbConnect from "@/db/connect";
+import Users from "@/db/models/users";
 
 export async function createUser(newUser) {
   try {
     const db = await dbConnect();
 
-    const usersCollection = db.collection("users");
+    if (!db) {
+      throw new Error("Failed to connect to the database");
+    }
 
-    // create new user
-    const result = await usersCollection.insertOne(newUser);
+    const existingUser = await Users.findById(newUser._id);
 
+    if (existingUser) {
+      return null; // User already exists
+    }
+    // Create new user
+    const createdUser = await Users.create(newUser);
     // return user / inserted documents (we only inserted one)
-    return result.ops[0];
+    return createdUser;
   } catch (error) {
     console.error("error", error);
   }
