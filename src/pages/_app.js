60122adfe1b07dfe0b873deaa3c0d94cfaea2eed
mntzd <julien.leske@gmail.com>
2023-07-05 11:51:53 +0200

    removed datafetch and useEffect to Update Zustands from _app and restructured the userLocation and Header Component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,14 @@
 import "@/styles/globals.css";
 import { roboto_condensed, raleway } from "@/styles/fonts";
-import useSWR, { SWRConfig } from "swr";
-import { useEffect, useState } from "react";
-import useStore from "./globalstores";
+import { SWRConfig } from "swr";
+import { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 import { createGlobalStyle } from "styled-components";
-import Loader from "@/components/Loader/Loader";
 import { useRouter } from "next/router";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps, session, status }) {
-  const setUsersData = useStore((state) => state.setUsersData);
-  const setGlobalProductCounter = useStore(
-    (state) => state.setGlobalProductCounter
-  );
-
   const router = useRouter();
 
   useEffect(() => {
@@ -24,19 +17,6 @@ export default function App({ Component, pageProps, session, status }) {
       router.push("/auth/signin");
     }
   }, []);
-
-  const { data, error, isLoading } = useSWR("/api/users", fetcher);
-
-  useEffect(() => {
-    console.log("reloaded in app");
-    if (data) {
-      setUsersData(data);
-      data && setGlobalProductCounter(data.productCounter);
-    }
-  }, [data]);
-
-  if (error) return <div>failed to load</div>;
-  if (isLoading) return <Loader />;
 
   return (
     <SessionProvider session={session}>
